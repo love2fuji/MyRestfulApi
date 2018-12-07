c692a03976a967b00984e741b5b2a709bb0ae875
xiaoshi WEN <love2fuji@outlook.com>
2018-12-07 17:01:16 +0800

    添加 User

---
+++ MyRestfulApi/Data/MyRestfulApiContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace MyRestfulApi.Models
+{
+    public class MyRestfulApiContext : DbContext
+    {
+        public MyRestfulApiContext (DbContextOptions<MyRestfulApiContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MyRestfulApi.Models.User> User { get; set; }
+    }
+}
